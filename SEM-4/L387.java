b3436c48bd5b21ba01ddb68dbd8194e351967445
CosmicCoder06 <k.avhi4612@gmail.com>
2026-01-27 11:30:16 +0530

    DSA question practice

--- SEM-4/L387.java
+++ SEM-4/L387.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 public class L387 {
-
     public static int L387(String s) {
-       
-    }
+        int[] freq = new int[256];
 
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i)] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+        int result = L387(s);
+        System.out.println("First non-repeating character's index: " + result);
+        sc.close();
+    }
 }
