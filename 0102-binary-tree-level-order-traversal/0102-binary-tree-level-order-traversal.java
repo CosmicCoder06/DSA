06689c8eeea2817dc84372f82a20c26059f8d46c
Abhinav Yadav <k.avhi4612@gmail.com>
2026-04-21 11:29:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,26 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                level.add(node.val);
-
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-
-            result.add(level);
+     Queue<TreeNode> q = newLinkedList<>();
+     q.offer(root);
+     while(!qisEmpty()){
+        TreeNode cur = q.peek();
+        List<Integer> curr = new ArrayList<>();
+        int lvlSize = q.size();
+        while(lvlSize > 0){
+            curr.add(q.peek().val);
+            if(q.peek().left != null)q.offer(q.peek().left);
+            if(q.peek().right != null)q.offer(q.peek().right);
+            lvlSize--;
+            q.peek();
         }
-
-        return result;
+        res.add(curr);
+     } 
     }
 }
